a5c2ffa4ef73dc8202447e35103bc7935945d7c4
Adamo2499 <adambieszk6@gmail.com>
2019-04-03 14:48:47 +0200

    Upload other files
    Upload voices, sfx, .css and .js files and index.html

---
+++ skrypty/muzyka.js
@@ -0,0 +1,125 @@
+var dzwiek = document.getElementById("audio");
+
+//Cult to Follow
+
+var album1 = document.getElementById("album1");
+var album2 = document.getElementById("album2");
+var ctf1 = document.getElementById("ctf1");
+var ctf2 = document.getElementById("ctf2");
+var ctf3 = document.getElementById("ctf3");
+var ctf4 = document.getElementById("ctf4");
+var ctf5 = document.getElementById("ctf5");
+var ctf6 = document.getElementById("ctf6");
+var ctf7 = document.getElementById("ctf7");
+var ctf8 = document.getElementById("ctf8");
+var ctf9 = document.getElementById("ctf9");
+var ctf10 = document.getElementById("ctf10");
+
+//Speciale 
+
+var wiadomosc1 = document.getElementById("wiadomosc1");
+var wiadomosc2 = document.getElementById("wiadomosc2");
+var wiadomosc3 = document.getElementById("wiadomosc3");
+var wiadomosc4 = document.getElementById("wiadomosc4");
+
+//Slider
+
+var s1 = document.getElementById("s1");
+var s2 = document.getElementById("s2");
+var s3 = document.getElementById("s3");
+var s4 = document.getElementById("s4");
+var s5 = document.getElementById("s5");
+var s6 = document.getElementById("s6");
+var s7 = document.getElementById("s7");
+var s8 = document.getElementById("s8");
+var s9 = document.getElementById("s9");
+var s10 = document.getElementById("s10");
+
+
+function klik(){ dzwiek.play(); }
+
+function album1_start(){ album1.play(); };
+
+function album1_stop(){ album1.pause(); };
+
+function album2_start(){ album2.play(); };
+
+function album2_stop(){	album2.pause(); };
+
+function wiadomosc1_start(){ wiadomosc1.play(); };
+
+function wiadomosc1_stop(){ wiadomosc1.pause(); };
+
+function wiadomosc2_start(){ wiadomosc2.play(); };
+
+function wiadomosc2_stop(){ wiadomosc2.pause(); };
+
+function wiadomosc3_start(){ wiadomosc3.play(); };
+
+function wiadomosc3_stop(){ wiadomosc3.pause(); };
+
+function wiadomosc4_start(){ wiadomosc4.play(); };
+
+function wiadomosc4_stop(){ wiadomosc4.pause(); };
+
+function ctf1_start(){ ctf1.play(); };
+function ctf1_stop(){ ctf1.pause(); };
+
+function ctf2_start(){ ctf2.play(); };
+function ctf2_stop(){ ctf2.pause(); };
+
+function ctf3_start(){ ctf3.play(); };
+function ctf3_stop(){ ctf3.pause(); };
+
+function ctf4_start(){ ctf4.play(); };
+function ctf4_stop(){ ctf4.pause(); };
+
+function ctf5_start(){ ctf5.play(); };
+function ctf5_stop(){ ctf5.pause(); };
+
+function ctf6_start(){ ctf6.play(); };
+function ctf6_stop(){ ctf6.pause(); };
+
+function ctf7_start(){ ctf7.play(); };
+function ctf7_stop(){ ctf7.pause(); };
+
+function ctf8_start(){ ctf8.play(); };
+function ctf8_stop(){ ctf8.pause(); };
+
+function ctf9_start(){ ctf9.play(); };
+function ctf9_stop(){ ctf9.pause(); };
+
+function ctf10_start(){ ctf10.play(); };
+function ctf10_stop(){ ctf10.pause(); };
+
+
+function s1_start(){ s1.play(); };
+function s1_stop(){ s1.pause(); };
+
+function s2_start(){ s2.play(); };
+function s2_stop(){ s2.pause(); };
+
+function s3_start(){ s3.play(); };
+function s3_stop(){ s3.pause(); };
+
+function s4_start(){ s4.play(); };
+function s4_stop(){ s4.pause(); };
+
+function s5_start(){ s5.play(); };
+function s5_stop(){ s5.pause(); };
+
+function s6_start(){ s6.play(); };
+function s6_stop(){ s6.pause(); };
+
+function s7_start(){ s7.play(); };
+function s7_stop(){ s7.pause(); };
+
+function s8_start(){ s8.play(); };
+function s8_stop(){ s8.pause(); };
+
+function s9_start(){ s9.play(); };
+function s9_stop(){ s9.pause(); };
+
+function s10_start(){ s10.play(); };
+function s10_stop(){ s10.pause(); };
+
